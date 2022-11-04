a0741269e73d5bcc3b336b0848cef3cfbb9b184f
fanstuck <62112487+Fanstuck@users.noreply.github.com>
2022-11-04 11:15:51 +0800

    Create SQLColumnVisual.py
    Get the Columns blood relationship visualization of SQL statement

---
+++ sqlparse/SQLColumnVisual.py
@@ -0,0 +1,10 @@
+from MainDef import *
+
+if __name__ == '__main__':
+    sql=get_sqlstr('read_sql.txt')
+    stmt_tuple=analysis_statements(sql)
+    for each_stmt in stmt_tuple:
+        type_name = get_main_functionsql(each_stmt)
+        blood_table(each_stmt)
+        blood_column(each_stmt)
+        column_visus()
